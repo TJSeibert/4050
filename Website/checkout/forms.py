f0aac7493f755580eaeecd3eee2dc55f7368f6f3
Ash Walker <ash@ashwalker.net>
2019-12-03 20:19:03 -0500

    Promo code & payment info saving now optional in checkout

--- Website/checkout/forms.py
+++ Website/checkout/forms.py
@@ -3,9 +3,9 @@ from django.core.validators import RegexValidator
 
 
 class CheckoutForm(forms.Form):
-    billing_address = forms.CharField()
+    billing_address = forms.CharField(label="Billing Address")
     card_type = forms.ChoiceField(choices=[("MC", "MasterCard"), ("VS", "Visa")])
-    card_number = forms.RegexField(regex="\\d{16}")
-    expiration = forms.RegexField(regex="[01][1-9]/[0-9][0-9]")
-    promo_code = forms.CharField()
-    save = forms.BooleanField()
+    card_number = forms.RegexField(regex="\\d{16}", label="Card Number")
+    expiration = forms.RegexField(regex="[01][1-9]/[0-9][0-9]", label="Expiration Date")
+    promo_code = forms.CharField(required=False, label="Promo Code")
+    save = forms.BooleanField(required=False, label="Save Payment Information")
